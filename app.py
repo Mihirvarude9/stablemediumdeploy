de7eed02d48edfa4537cf6fac7e5ca6c5112a5d6
Mihirvarude9 <mvarude09@gmail.com>
2025-06-24 17:16:52 +0530

    changes

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Request, HTTPException 
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from uuid import uuid4
-from diffusers import SD3Transformer2DModel, StableDiffusion3Pipeline
+from diffusers import BitsAndBytesConfig, SD3Transformer2DModel, StableDiffusion3Pipeline
 import torch
 import os
 
@@ -13,18 +13,26 @@ API_KEY = "wildmind_5879fcd4a8b94743b3a7c8c1a1b4"
 OUTPUT_DIR = "generated"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-# === LOAD MODEL (GPU OPTIMIZED) ===
-model = SD3Transformer2DModel.from_pretrained(
+# === LOAD MODEL (Stable Medium with NF4 Quantization) ===
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16
+)
+
+model_nf4 = SD3Transformer2DModel.from_pretrained(
     model_id,
     subfolder="transformer",
+    quantization_config=nf4_config,
     torch_dtype=torch.float16
-).to("cuda")
+)
 
 pipeline = StableDiffusion3Pipeline.from_pretrained(
     model_id,
-    transformer=model,
+    transformer=model_nf4,
     torch_dtype=torch.float16
-).to("cuda")
+)
+pipeline.enable_model_cpu_offload()
 
 # === FASTAPI SETUP ===
 app = FastAPI()
@@ -56,7 +64,12 @@ async def generate_medium(request: Request, body: PromptRequest):
     if not prompt:
         raise HTTPException(status_code=400, detail="Prompt is empty")
 
-    image = pipeline(prompt=prompt, num_inference_steps=50, guidance_scale=5.5).images[0]
+    image = pipeline(
+        prompt=prompt,
+        num_inference_steps=60,
+        guidance_scale=5.5,
+    ).images[0]
+
     filename = f"{uuid4().hex}.png"
     filepath = os.path.join(OUTPUT_DIR, filename)
     image.save(filepath)
